66cea9ac9302870c32876c176e457fd32e86942b
clara-j <clara-j@users.noreply.github.com>
2019-11-01 16:46:33 -0400

    Added verbose mode

--- pfsense_zfs_check.sh
+++ pfsense_zfs_check.sh
@@ -1,26 +1,58 @@
 #!/bin/sh
 
+
+#Modify this line if you want to change the allowed utilization for the pool
 max_capacity=50
+
+
 healthy_state="all pools are healthy"
+
+_V=0
+while getopts "v" OPTION
+do
+  case $OPTION in
+    v) _V=1
+       ;;
+  esac
+done
 
 
 #Check health status of drive
 pool_status=`zpool status -x`
+
+if [ $_V -eq 1  ]; then
+    echo "--------------------------"
+    echo "------  Pool Status  -----"
+    echo "--------------------------"
+    zpool status
+fi
+
+
 if [ "$pool_status" = "$healthy_state"  ]; then
-    echo "All pools are OK"
+    echo "------  All pools are OK -"
+    echo ""
+    echo "------  Starting Scrubs  -"
 
     #Trigger Scrub again for drives
     zpool list -H |  while read line; do
         pool=`echo "$line" | cut -f1`
         echo "Triggering scrub for: $pool"
-        zpool scrub "$pool"
+        #zpool scrub "$pool"
     done
 
 else
-    echo "There is an error with one of the pools"
+    echo "!! There is an error with one of the pools  !!"
 
     #Send email warning
     echo "$pool_status" | /usr/local/bin/php /usr/local/bin/mail.php -s"`hostname`: pfSense zpool warning - Error"
+fi
+
+echo ""
+echo ""
+if [ $_V -eq 1  ]; then
+    echo "--------------------------"
+    echo "------  Checking Space  --"
+    echo "--------------------------"
 fi
 
 
